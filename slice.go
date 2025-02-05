a168e6afc3da65773a41e69271a4d8d3449d72ec
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-02-05 11:05:22 +0700

    add new slice practice

--- slice.go
+++ slice.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	names := [...]string{
+	names := [...]string {
 		"Arya",
 		"Rizki",
 		"Andaru",
@@ -20,4 +20,51 @@ func main() {
 
 	slice3 := names[0:]
 	fmt.Println(slice3)
+
+	days := [...]string {
+		"Senin",
+		"Selasa",
+		"Rabu",
+		"Kamis",
+		"Jumat",
+		"Sabtu",
+		"Minggu",
+	}
+
+	daySlice1 := days[5:]
+	daySlice1[0] = "Sabtu Baru"
+	daySlice1[1] = "Minggu Baru"
+	fmt.Println(daySlice1)
+	fmt.Println(days)
+
+	daySlice2 := append(daySlice1, "Libur Baru")
+	daySlice2[0] = "Sabtu Lama"
+	fmt.Println(daySlice1)
+	fmt.Println(daySlice2)
+	fmt.Println(days)
+
+	newSlice := make([]string, 2, 5)
+	newSlice[0] = "Arya"
+	newSlice[1] = "Arya"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Arya")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Budi"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
 }
